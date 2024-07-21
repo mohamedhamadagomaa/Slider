dd44d97c65ff64a8b6141cf9b509c611dbdfee9a
Mohamed Hamada <moha2016226@gmail.com>
2024-07-21 09:26:05 -0700

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import Carousel from "./Carousel";
+import SlickCarousel from "./SlickCarousel";
+
+const App = () => {
+  
+  return (
+    <main>
+      <SlickCarousel />
+    </main>
+  );
+};
+export default App;
